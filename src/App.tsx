026ec78363152f1bb95e65f74a943132b0c48533
leoleolro <leo804541185@gmail.com>
2024-02-19 15:18:27 +0800

    customised for NINI

--- src/App.tsx
+++ src/App.tsx
@@ -12,55 +12,88 @@ export default function Page() {
 
   const getNoButtonText = () => {
     const phrases = [
-      "No",
-      "Are you sure?",
-      "Really sure?",
-      "Think again!",
-      "Last chance!",
-      "Surely not?",
-      "You might regret this!",
-      "Give it another thought!",
-      "Are you absolutely certain?",
-      "This could be a mistake!",
-      "Have a heart!",
-      "Don't be so cold!",
-      "Change of heart?",
-      "Wouldn't you reconsider?",
-      "Is that your final answer?",
-      "You're breaking my heart ;(",
+      "我不",
+      "真哒?",
+      "你确定?",
+      "在考虑考虑嘛!",
+      "再给你最后一次机会昂!",
+      "别?",
+      "二十年后当我们回想起这一幕，你会不会后悔：（",
+      "心都碎成二维码了，扫出来我还是好爱你呜呜呜",
+      "我们MBTI绝配啊",
+      "星座也绝配啊!",
+      "你没有心！！!",
+      "我只是一个可怜的小狗你忍心吗!",
+      "真不？",
+      "我biubiubiu你!",
+      "你这个冷酷无情的女人!",
+      "好 ;(",
     ];
 
     return phrases[Math.min(noCount, phrases.length - 1)];
   };
 
   return (
-    <div className="centered-container">      
+    <div className="centered-container">
       <div className="valentine-container">
         {yesPressed ? (
           <>
-            <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
-            <div className="text-container">Ok yay!!!</div>
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              <img src="https://media.tenor.com/PS4dJbjo7mIAAAAi/heart-love.gif" />
+              <img src="https://media1.tenor.com/m/62YQCrZkjiQAAAAC/yellow-pochacco.gif" />
+            </div>
+            <div className="text-container">芜湖起飞飞飞飞!!!</div>
           </>
         ) : (
           <>
-            <img
-              className="h-[200px]"
-              style={{ width: "400x", height: "240px" }}
-              src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
-            />
-            <h1 className="text-container">Will you be my Valentine?</h1>
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              <img
+                className="h-[200px]"
+                style={{ width: "400x", height: "240px" }}
+                src="https://media1.tenor.com/m/HA61HwAtP8wAAAAC/pochacco-sanrio.gif"
+              />
+              <img
+                className="h-[200px]"
+                style={{ width: "400x", height: "240px" }}
+                src="https://media1.tenor.com/m/zZnfFImvNj4AAAAC/cinnamoroll-cappuccino.gif"
+              />
+              <img
+                className="h-[200px]"
+                style={{ width: "400x", height: "240px" }}
+                src="https://media1.tenor.com/m/nSeXvWhdjPEAAAAC/pochacco-happy.gif"
+              />
+            </div>
+
+            <h1 className="text-container">蒋雯妮情人节快乐，所以要不要做我女朋友?</h1>
             <div>
               <button
                 className={"yes-button"}
                 style={{ fontSize: yesButtonSize }}
                 onClick={() => setYesPressed(true)}
               >
-                Yes
+                哦ker
               </button>
 
               <button onClick={handleNoClick} className="no-button">
-                {noCount === 0 ? "No" : getNoButtonText()}
+                {noCount === 0 ? "狠心残忍冰心水泥封心大反派拒绝" : getNoButtonText()}
               </button>
+            </div>
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              <img
+                className="h-[200px]"
+                style={{ width: "400x", height: "240px" }}
+                src="https://media1.tenor.com/m/4OONA09xhs4AAAAC/pochacco-sleepy.gif"
+              />
+              <img
+                className="h-[200px]"
+                style={{ width: "400x", height: "240px" }}
+                src="https://media.tenor.com/ROK6T7ydNksAAAAi/verguenza-que.gif"
+              />
+              <img
+                className="h-[200px]"
+                style={{ width: "400x", height: "240px" }}
+                src="https://media1.tenor.com/m/UgHmWKl51EMAAAAC/puglie-puglie-pug.gif"
+              />
             </div>
           </>
         )}
